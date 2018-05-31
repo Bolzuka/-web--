7dc6ed44f0047f626d50d565cfc9223961141fa6
Shibaeva Maria <Bolzuka@gmail.com>
2018-05-31 09:33:20 +0300

    Add files via upload

---
+++ PC/08.05.2018/src/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallery = () =>
+	(<div>
+		<h1>Галерея</h1>
+		<p>Рисунки</p>
+        <img src='/Карта.png'  width="650" height="400" />
+        <img src='/Дата.png' width="650" height="400" />
+	</div>);
+
+export default Gallery;
---
+++ PC/08.05.2018/src/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Map = () =>
+	(
+	<div>
+		<h1>Карта</h1>
+		<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ae1baed9c03abe12e2b0ced8d3107d3fce40760babfd7f6ba281ceba4ae6d2b73&source=constructorLink;source=constructor" width="500" height="400" frameborder="0"></iframe>
+	</div>
+	);
+
+export default Map;
